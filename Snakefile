bc0a87dc4b6e89c9379fcabeaf077eef6bd95f0a
LukasFrankenQ <lukas.franken@ed.ac.uk>
2024-02-06 21:43:51 +0100

    added methods to build basic network

---
+++ Snakefile
@@ -0,0 +1,145 @@
+# SPDX-FileCopyrightText: : 2017-2023 The PyPSA-Eur Authors, Lukas Franken
+#
+# SPDX-License-Identifier: MIT
+
+
+import requests
+
+from os.path import normpath, exists
+from snakemake.remote.HTTP import RemoteProvider as HTTPRemoteProvider
+from snakemake.utils import min_version
+from datetime import datetime, timedelta
+
+HTTP = HTTPRemoteProvider()
+
+min_version("7.7")
+
+conf_file = os.path.join(workflow.current_basedir, "config/config.yaml")
+conf_default_file = os.path.join(workflow.current_basedir, "config/config.yaml")
+if not exists(conf_file) and exists(conf_default_file):
+    copyfile(conf_default_file, conf_file)
+
+
+configfile: "config/config.yaml"
+
+run = config.get("run", {})
+RDIR = run["name"] + "/" if run.get("name") else ""
+
+LOGS = "logs/" + RDIR
+BENCHMARKS = "benchmarks/" + RDIR
+
+RESOURCES = "resources/"
+RESULTS = "results/"
+
+
+# Check if the workflow has access to the internet by trying to access the HEAD of specified url
+def has_internet_access(url="www.zenodo.org") -> bool:
+    import http.client as http_client
+
+    # based on answer and comments from
+    # https://stackoverflow.com/a/29854274/11318472
+    conn = http_client.HTTPConnection(url, timeout=5)  # need access to zenodo anyway
+    try:
+        conn.request("HEAD", "/")
+        return True
+    except:
+        return False
+    finally:
+        conn.close()
+
+if config["enable"].get("retrieve", "auto") == "auto":
+    config["enable"]["retrieve"] = has_internet_access()
+
+if config["enable"]["retrieve"] is False:
+    print("Datafile downloads disabled in config[retrieve] or no internet access.")
+
+
+if config["enable"]["retrieve"] and config["enable"].get("retrieve_databundle", True):
+    datafiles = [
+        "ch_cantons.csv",
+        "je-e-21.03.02.xls",
+        "eez/World_EEZ_v8_2014.shp",
+        "hydro_capacities.csv",
+        "naturalearth/ne_10m_admin_0_countries.shp",
+        "NUTS_2013_60M_SH/data/NUTS_RG_60M_2013.shp",
+        "nama_10r_3popgdp.tsv.gz",
+        "nama_10r_3gdp.tsv.gz",
+        "corine/g250_clc06_V18_5.tif",
+        "natura/Natura2000_end2015.shp",
+        "GEBCO_2014_2D.nc",
+    ]
+
+    rule retrieve_databundle:
+        output:
+            protected(expand("data/bundle/{file}", file=datafiles)),
+        log:
+            LOGS + "retrieve_databundle.log",
+        resources:
+            mem_mb=1000,
+        # retries: 0
+        conda:
+            "envs/retrieve.yaml"
+        script:
+            "scripts/retrieve_databundle.py"
+
+
+rule build_shapes:
+    params:
+        countries=config["countries"],
+    input:
+        naturalearth=ancient("data/bundle/naturalearth/ne_10m_admin_0_countries.shp"),
+        eez=ancient("data/bundle/eez/World_EEZ_v8_2014.shp"),
+        nuts3=ancient("data/bundle/NUTS_2013_60M_SH/data/NUTS_RG_60M_2013.shp"),
+        nuts3pop=ancient("data/bundle/nama_10r_3popgdp.tsv.gz"),
+        nuts3gdp=ancient("data/bundle/nama_10r_3gdp.tsv.gz"),
+        ch_cantons=ancient("data/bundle/ch_cantons.csv"),
+        ch_popgdp=ancient("data/bundle/je-e-21.03.02.xls"),
+    output:
+        country_shapes=RESOURCES + "country_shapes.geojson",
+        offshore_shapes=RESOURCES + "offshore_shapes.geojson",
+        europe_shape=RESOURCES + "europe_shape.geojson",
+        nuts3_shapes=RESOURCES + "nuts3_shapes.geojson",
+    log:
+        LOGS + "build_shapes.log",
+    threads: 1
+    resources:
+        mem_mb=1500,
+    conda:
+        "envs/environment.yaml"
+    script:
+        "scripts/build_shapes.py"
+
+
+
+rule base_network:
+    params:
+        countries=config["countries"],
+        # snapshots={k: config["snapshots"][k] for k in ["start", "end", "inclusive"]},
+        lines=config["lines"],
+        links=config["links"],
+        transformers=config["transformers"],
+    input:
+        eg_buses="data/entsoegridkit/buses.csv",
+        eg_lines="data/entsoegridkit/lines.csv",
+        eg_links="data/entsoegridkit/links.csv",
+        eg_converters="data/entsoegridkit/converters.csv",
+        eg_transformers="data/entsoegridkit/transformers.csv",
+        parameter_corrections="data/parameter_corrections.yaml",
+        links_p_nom="data/links_p_nom.csv",
+        links_tyndp="data/links_tyndp.csv",
+        country_shapes=RESOURCES + "country_shapes.geojson",
+        offshore_shapes=RESOURCES + "offshore_shapes.geojson",
+        europe_shape=RESOURCES + "europe_shape.geojson",
+    output:
+        RESOURCES + "networks/base.nc",
+    log:
+        LOGS + "base_network.log",
+    benchmark:
+        BENCHMARKS + "base_network"
+    threads: 1
+    resources:
+        mem_mb=1500,
+    conda:
+        "envs/environment.yaml"
+    script:
+        "scripts/base_network.py"
---
+++ scripts/_helpers.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: : 2017-2023 The PyPSA-Eur Authors
+#
+# SPDX-License-Identifier: MIT
+
+
+import os
+import urllib
+import hashlib
+import requests
+import contextlib
+
+from tqdm import tqdm
+
+from pathlib import Path
+
+# Define a context manager to temporarily mute print statements
+@contextlib.contextmanager
+def mute_print():
+    with open(os.devnull, "w") as devnull:
+        with contextlib.redirect_stdout(devnull):
+            yield
+
+
+def configure_logging(snakemake, skip_handlers=False):
+    """
+    Configure the basic behaviour for the logging module.
+
+    Note: Must only be called once from the __main__ section of a script.
+
+    The setup includes printing log messages to STDERR and to a log file defined
+    by either (in priority order): snakemake.log.python, snakemake.log[0] or "logs/{rulename}.log".
+    Additional keywords from logging.basicConfig are accepted via the snakemake configuration
+    file under snakemake.config.logging.
+
+    Parameters
+    ----------
+    snakemake : snakemake object
+        Your snakemake object containing a snakemake.config and snakemake.log.
+    skip_handlers : True | False (default)
+        Do (not) skip the default handlers created for redirecting output to STDERR and file.
+    """
+    import logging
+    import sys
+
+    kwargs = snakemake.config.get("logging", dict()).copy()
+    kwargs.setdefault("level", "INFO")
+
+    if skip_handlers is False:
+        fallback_path = Path(__file__).parent.joinpath(
+            "..", "logs", f"{snakemake.rule}.log"
+        )
+        logfile = snakemake.log.get(
+            "python", snakemake.log[0] if snakemake.log else fallback_path
+        )
+        kwargs.update(
+            {
+                "handlers": [
+                    # Prefer the 'python' log, otherwise take the first log for each
+                    # Snakemake rule
+                    logging.FileHandler(logfile),
+                    logging.StreamHandler(),
+                ]
+            }
+        )
+    logging.basicConfig(**kwargs)
+
+    # Setup a function to handle uncaught exceptions and include them with their stacktrace into logfiles
+    def handle_exception(exc_type, exc_value, exc_traceback):
+        # Log the exception
+        logger = logging.getLogger()
+        logger.error(
+            "Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback)
+        )
+
+    sys.excepthook = handle_exception
+
+
+def progress_retrieve(url, file, disable=False):
+    if disable:
+        urllib.request.urlretrieve(url, file)
+    else:
+        with tqdm(unit="B", unit_scale=True, unit_divisor=1024, miniters=1) as t:
+
+            def update_to(b=1, bsize=1, tsize=None):
+                if tsize is not None:
+                    t.total = tsize
+                t.update(b * bsize - t.n)
+
+            urllib.request.urlretrieve(url, file, reporthook=update_to)
+
+
+def get_checksum_from_zenodo(file_url):
+    parts = file_url.split("/")
+    record_id = parts[parts.index("record") + 1]
+    filename = parts[-1]
+
+    response = requests.get(f"https://zenodo.org/api/records/{record_id}", timeout=30)
+    response.raise_for_status()
+    data = response.json()
+
+    for file in data["files"]:
+        if file["key"] == filename:
+            return file["checksum"]
+    return None
+
+
+def validate_checksum(file_path, zenodo_url=None, checksum=None):
+    """
+    Validate file checksum against provided or Zenodo-retrieved checksum.
+    Calculates the hash of a file using 64KB chunks. Compares it against a
+    given checksum or one from a Zenodo URL.
+
+    Parameters
+    ----------
+    file_path : str
+        Path to the file for checksum validation.
+    zenodo_url : str, optional
+        URL of the file on Zenodo to fetch the checksum.
+    checksum : str, optional
+        Checksum (format 'hash_type:checksum_value') for validation.
+
+    Raises
+    ------
+    AssertionError
+        If the checksum does not match, or if neither `checksum` nor `zenodo_url` is provided.
+
+
+    Examples
+    --------
+    >>> validate_checksum("/path/to/file", checksum="md5:abc123...")
+    >>> validate_checksum(
+    ...     "/path/to/file",
+    ...     zenodo_url="https://zenodo.org/record/12345/files/example.txt",
+    ... )
+
+    If the checksum is invalid, an AssertionError will be raised.
+    """
+    assert checksum or zenodo_url, "Either checksum or zenodo_url must be provided"
+    if zenodo_url:
+        checksum = get_checksum_from_zenodo(zenodo_url)
+    hash_type, checksum = checksum.split(":")
+    hasher = hashlib.new(hash_type)
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(65536), b""):  # 64kb chunks
+            hasher.update(chunk)
+    calculated_checksum = hasher.hexdigest()
+    assert (
+        calculated_checksum == checksum
+    ), "Checksum is invalid. This may be due to an incomplete download. Delete the file and re-execute the rule."
